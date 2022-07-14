4cad6479f6d8e01abedcbb7602b196b17f564c71
Kate Higa <khiga8@github.com>
2022-07-14 10:58:27 -0700

    use iife format

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
   input: "./src/index.js",
   output: {
     file: "./build/bundle.min.js",
-    format: "es",
+    format: "iife",
     name: "bundle",
   },
   plugins: [nodeResolve()],
