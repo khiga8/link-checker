e2f3b2d1b4285fbae33210ccaa9cb053acefa42d
Kate Higa <khiga8@github.com>
2022-07-14 11:24:24 -0700

    Update package.json

--- generate-bookmarklet.js
+++ generate-bookmarklet.js
@@ -4,8 +4,9 @@ const code = fs.readFileSync("build/bundle.min.js", "utf-8");
 const output =
   "javascript:" + encodeURIComponent("(function(){" + code.trim() + "})();");
 
-fs.writeFileSync("index.html",
-`<h1>Link Checker Bookmarklet</h1>
+fs.writeFileSync(
+  "index.html",
+  `<h1>Link Checker Bookmarklet</h1>
 <p>
   Drag the following link to your bookmarks bar:
   <a href="${output}">Link Checker</a>
